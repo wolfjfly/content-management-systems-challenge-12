9d7ba7bb71acadc4ce53e5cbdea51edb488aa8d2
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-11-19 14:26:06 -0600

    finished schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
@@ -21,6 +21,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
