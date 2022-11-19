39facea4e28e0ab3852f6f1eeb02cda8697d4b02
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-11-19 17:40:06 -0600

    Veiw All Employees in a table works

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
-const inquirer= require('inquirer');
-const cTable= require('console.table');
+const inquirer = require('inquirer');
+require('console.table');
 const mysql = require('mysql2');
-
-require('dotenv').config();
-
-//Database configuration
-const dbConfig = {
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: process.env.DB_NAME
-}
+const dbConfig= require('./config/config')
 
 // Connect to database
 const db = mysql.createConnection(dbConfig,
-    console.log(` +++ Connected to ${process.env.DB_NAME}`)
-).promise(); // <== Enable Promise based syntax
+    console.log(` +++ Connected to ${process.env.DB_NAME}`))
+
+const getAllEmployee=()=>{
+    return db.promise().query('SELECT * FROM employee')
+}
+
+
+
+
+const menu=()=>{
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do"',
+                name: 'main',
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'Veiw All Roles','Add Role', 'Veiw All Departments','Add Department'],
+            },
+        ])
+        .then((data) => {
+            if(data.main==='View All Employees'){
+                getAllEmployee().then((employee)=>{
+                    console.table(employee[0])
+                return menu();
+                })
+            
+            }else if (data.main==='Add Employee'){
+
+            }else if (data.main==='Update Employee Role'){
+
+            }else if (data.main==='Veiw All Roles'){
+
+            }else if (data.main==='Add Role'){
+
+            }else if (data.main==='Veiw All Departments'){
+
+            }else if (data.main==='Add Department'){
+
+            }
+    });
+}
+menu()
